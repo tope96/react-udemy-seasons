6458b341dc47ec36feb848b2bcf22890f2add626
tope96 <pietrzaktomek@hotmail.com>
2019-08-22 22:42:36 +0200

    to 58

--- src/index.js
+++ src/index.js
@@ -4,18 +4,26 @@ import ReactDOM from 'react-dom';
 class App extends React.Component {
     constructor(props){
         super(props);
-        this.state = { lat: null };
+        this.state = { lat: null, errorMessage: "" };
 
         window.navigator.geolocation.getCurrentPosition(
             position => {
-                this.setState({ lat: position.coords.latitude })
+                this.setState({ lat: position.coords.latitude });
             },
-            err => console.log(err)
+            err => {
+                this.setState({errorMessage: "Cos poszlo nie tak :("});
+            }
         );
     }
 
     render() {
-        return <div> Latitude: {this.state.lat} </div>;
+        return (
+        <div>
+            Latitude: {this.state.lat}
+            <br />
+            Error: {this.state.errorMessage}
+        </div>
+        );
     }
 }
 
